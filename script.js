651895f261cea273e55d53d39330d540b7047f35
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2023-10-13 18:30:52 -0300

    Mudando as cores do checkbox

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const modal = document.querySelector('dialog')
 const modalClose = document.querySelector('dialog button')
 
 checkBoxMode.addEventListener('click', () => {
-
+    
     let oldTheme = localStorage.getItem("themeSystem") || "light"
     let newTheme = oldTheme == "light" ? "dark" : "light"
 
@@ -88,12 +88,17 @@ function showTask (){
     let newList = ''
     
     taskList.forEach((item, index) =>{
-    
-        const iconSrc = item.done ? './src/check.png' : './src/unCheck.png';
+
+        const checkboxsvg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path></svg>'
+        const checked = '<svg class="greenchk" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H7zm4 10.414-2.707-2.707 1.414-1.414L11 12.586l3.793-3.793 1.414 1.414L11 15.414z"></path></svg>'
+        const iconSrc = item.done ? checked : checkboxsvg;
+
+        
+
 
     newList = newList + `
         <li class="tasks ${item.done && "feito"}">
-            <img class="check-icon" src="${iconSrc}" alt="check-icon" onclick="checkTask(${index})">
+            <span class="check-icon ${item.done && "verde"}"  onclick="checkTask(${index})">${iconSrc}</span>
             <p>${item.tarefa}</p>
             <img class="delete-icon" src="./src/delete.png" alt="unCheck-icon" onclick="deletarItem(${index})">
         </li>
